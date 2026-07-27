57a930f76b1e37d875474a8fac2a11af56e33600
muni <munisekhark11@gmail.com>
2026-07-28 00:41:27 +0530

    JSX & rules

---
+++ jsx/README.md
@@ -0,0 +1,13 @@
+#  rules of jsx
+
+### every tag should have parent
+### every tag must be closed
+### Use className, not class
+### Use camelCase for attributes
+### Use curly braces for javaScript
+
+
+## JSX
+import {createRoot} from "react-dom/client"
+const root=createRoot(document.getElementByID("root));
+root.render();
---
+++ jsx/src/main.jsx
@@ -0,0 +1,30 @@
+// //import architect from mumbai
+ import {createRoot} from "react-dom/client"
+import "./index.css"
+// //finding a land
+// const root=createRoot(document.getElementId("root"));
+
+
+
+// //Start Building
+// root.render();
+//import architect from mumbai
+
+const m="muni";
+const d=new Date();
+
+//finding a land
+createRoot(document.getElementById("root")).render(
+    <>
+        <h1 className="head">Hello World om namaha shivaya {m.toUpperCase()}</h1>
+    <h2> {88+88} Today date is {d.getDate()}</h2>
+    </>
+
+    
+    
+    
+);
+
+
+
+
